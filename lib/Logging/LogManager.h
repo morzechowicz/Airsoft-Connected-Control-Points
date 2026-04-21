2fc0c0fe00d01506f452c8427cc87eb390a29157
Michal <michal@orzechowicz.pl>
2026-04-21 16:27:36 +0200

    make ble logging into seperate task so it wont clog bandwith so easly

--- lib/Logging/LogManager.h
+++ lib/Logging/LogManager.h
@@ -7,37 +7,46 @@
 #include <vector>
 #include <functional>
 
+QueueHandle_t bleLogQueue;
+
 // Log levels
-enum LogLevel {
+enum LogLevel
+{
     LOG_DEBUG = 0,
     LOG_INFO = 1,
     LOG_WARN = 2,
     LOG_ERROR = 3,
-    LOG_NONE = 4  // Disable all logging
+    LOG_NONE = 4 // Disable all logging
 };
 
 // Output flags
-#define LOG_OUTPUT_SERIAL  0x01
-#define LOG_OUTPUT_BLE     0x02
-#define LOG_OUTPUT_LORA    0x04
+#define LOG_OUTPUT_SERIAL 0x01
+#define LOG_OUTPUT_BLE 0x02
+#define LOG_OUTPUT_LORA 0x04
 
 // Colors for serial output (ANSI escape codes)
-#define LOG_COLOR_RESET   "\033[0m"
-#define LOG_COLOR_DEBUG   "\033[36m"  // Cyan
-#define LOG_COLOR_INFO    "\033[32m"  // Green
-#define LOG_COLOR_WARN    "\033[33m"  // Yellow
-#define LOG_COLOR_ERROR   "\033[31m"  // Red
+#define LOG_COLOR_RESET "\033[0m"
+#define LOG_COLOR_DEBUG "\033[36m" // Cyan
+#define LOG_COLOR_INFO "\033[32m"  // Green
+#define LOG_COLOR_WARN "\033[33m"  // Yellow
+#define LOG_COLOR_ERROR "\033[31m" // Red
 
-class LogManager {
+class LogManager
+{
 public:
-    static LogManager& getInstance() {
+    static LogManager &getInstance()
+    {
         static LogManager instance;
         return instance;
     }
-    
+
     // Initialization
     void begin(LogLevel level = LOG_INFO, uint8_t outputs = LOG_OUTPUT_SERIAL);
-    
+
+    // ble queue task
+    void bleLogTask(void *pvParameters);
+    void createBleLogTask();
+
     // Configuration
     void setLogLevel(LogLevel level);
     void setOutputs(uint8_t outputs);
@@ -45,54 +54,54 @@ public:
     void setTimestamps(bool enable);
     void setTags(bool enable);
     void setBLEStatus(bool connected);
-    
+
     // Output callbacks (for BLE/LoRa)
-    void setBLECallback(std::function<void(const char*)> callback);
-    void setLoRaCallback(std::function<void(const char*)> callback);
-    
+    void setBLECallback(std::function<void(const char *)> callback);
+    void setLoRaCallback(std::function<void(const char *)> callback);
+
     // Main logging methods
-    void log(LogLevel level, const char* tag, const char* format, ...);
-    void logRaw(const char* format, ...);  // No formatting, just output
-    
+    void log(LogLevel level, const char *tag, const char *format, ...);
+    void logRaw(const char *format, ...); // No formatting, just output
+
     // Convenience methods
-    void debug(const char* tag, const char* format, ...);
-    void info(const char* tag, const char* format, ...);
-    void warn(const char* tag, const char* format, ...);
-    void error(const char* tag, const char* format, ...);
-    
+    void debug(const char *tag, const char *format, ...);
+    void info(const char *tag, const char *format, ...);
+    void warn(const char *tag, const char *format, ...);
+    void error(const char *tag, const char *format, ...);
+
     // Print methods (drop-in Serial replacement)
-    void print(const char* str);
+    void print(const char *str);
     void print(int val);
     void print(uint8_t val, int format = DEC);
-    void println(const char* str);
+    void println(const char *str);
     void println();
-    void printf(const char* format, ...);
-    
+    void printf(const char *format, ...);
+
 private:
     LogManager();
     ~LogManager();
-    
+
     // Prevent copying
-    LogManager(const LogManager&) = delete;
-    LogManager& operator=(const LogManager&) = delete;
-    
-    void output(const char* message, uint8_t outputs);
-    const char* getLevelColor(LogLevel level);
-    const char* getLevelString(LogLevel level);
-    
+    LogManager(const LogManager &) = delete;
+    LogManager &operator=(const LogManager &) = delete;
+
+    void output(const char *message, uint8_t outputs);
+    const char *getLevelColor(LogLevel level);
+    const char *getLevelString(LogLevel level);
+
     LogLevel currentLevel;
     uint8_t outputFlags;
     bool useColors;
     bool useTimestamps;
     bool useTags;
     bool BLEConnected;
-    
+
     SemaphoreHandle_t logMutex;
-    
-    std::function<void(const char*)> bleCallback;
-    std::function<void(const char*)> loraCallback;
-    
-    char buffer[512];  // Shared buffer for formatting
+
+    std::function<void(const char *)> bleCallback;
+    std::function<void(const char *)> loraCallback;
+
+    char buffer[512]; // Shared buffer for formatting
 };
 
 // Global convenience macros
@@ -100,33 +109,33 @@ private:
 
 // Conditional logging macros (compile-time removal)
 #ifdef LOG_ENABLE_DEBUG
-    #define LOGD(tag, ...) LOG.debug(tag, __VA_ARGS__)
+#define LOGD(tag, ...) LOG.debug(tag, __VA_ARGS__)
 #else
-    #define LOGD(tag, ...)
+#define LOGD(tag, ...)
 #endif
 
 #ifdef LOG_ENABLE_INFO
-    #define LOGI(tag, ...) LOG.info(tag, __VA_ARGS__)
+#define LOGI(tag, ...) LOG.info(tag, __VA_ARGS__)
 #else
-    #define LOGI(tag, ...)
+#define LOGI(tag, ...)
 #endif
 
 #ifdef LOG_ENABLE_WARN
-    #define LOGW(tag, ...) LOG.warn(tag, __VA_ARGS__)
+#define LOGW(tag, ...) LOG.warn(tag, __VA_ARGS__)
 #else
-    #define LOGW(tag, ...)
+#define LOGW(tag, ...)
 #endif
 
 #ifdef LOG_ENABLE_ERROR
-    #define LOGE(tag, ...) LOG.error(tag, __VA_ARGS__)
+#define LOGE(tag, ...) LOG.error(tag, __VA_ARGS__)
 #else
-    #define LOGE(tag, ...)
+#define LOGE(tag, ...)
 #endif
 
 // Always-on macros (runtime level check)
 #define LOG_DEBUG(tag, ...) LOG.debug(tag, __VA_ARGS__)
-#define LOG_INFO(tag, ...)  LOG.info(tag, __VA_ARGS__)
-#define LOG_WARN(tag, ...)  LOG.warn(tag, __VA_ARGS__)
+#define LOG_INFO(tag, ...) LOG.info(tag, __VA_ARGS__)
+#define LOG_WARN(tag, ...) LOG.warn(tag, __VA_ARGS__)
 #define LOG_ERROR(tag, ...) LOG.error(tag, __VA_ARGS__)
 
 #endif // LOG_MANAGER_H
