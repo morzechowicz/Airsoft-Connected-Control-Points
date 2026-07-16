d5f06dc9c1d23151e526e75c6576548119126db0
Michal <michal@orzechowicz.pl>
2026-07-16 19:52:19 +0200

    fix blinking led working incorrectly

--- src/GameComponents/KOTH/KOTHClient.cpp
+++ src/GameComponents/KOTH/KOTHClient.cpp
@@ -361,11 +361,13 @@ void KOTHClient::updateLEDs()
     {
         hardware->ledYellowButton.blink();
         hardware->ledBlueButton.blinkOff();
+        hardware->ledBlueButton.off();
     }
     else if (currentController == Team::BLUE)
     {
         hardware->ledBlueButton.blink();
         hardware->ledYellowButton.blinkOff();
+        hardware->ledYellowButton.off();
     }
     else
     {
