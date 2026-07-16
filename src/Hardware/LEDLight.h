762cd685a9129bc2cf85542ebd50071502baf593
morzechowicz <michal@orzechowicz.pl>
2026-07-16 19:31:22 +0200

    Revert "Audio visual"

--- src/Hardware/LEDLight.h
+++ src/Hardware/LEDLight.h
@@ -8,9 +8,6 @@ class LEDLight
 private:
     int pin;
     bool state;
-    bool blinking = false;
-    int blinkHz = 0;
-    long lastBlink = 0;
 
 public:
     LEDLight(int ledPin);
@@ -19,11 +16,6 @@ public:
     void off();
     void toggle();
     bool getState();
-    void blink(int herz = 200);
-
-    void blinkOff();
-
-    void update();
 };
 
 #endif // LED_H
